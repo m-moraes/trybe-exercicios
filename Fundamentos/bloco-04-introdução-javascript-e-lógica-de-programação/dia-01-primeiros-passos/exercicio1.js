341e482a10919c4307f1b9a106adc5fedd25bf64
Mariana Moraes <mari_a.s.p@hotmail.com>
2021-11-07 17:06:27 -0300

    Exercício 1 de javascript

---
+++ Fundamentos/bloco-04-introdução-javascript-e-lógica-de-programação/dia-01-primeiros-passos/exercicio1.js
@@ -0,0 +1,10 @@
+const myName = "Mariana";
+const birthCity = "Rio de Janeiro";
+let birthYear = 1995; 
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
